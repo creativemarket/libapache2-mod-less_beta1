1b9f9f66afe62b8ef3e63e74a7eb9dc68aa2f382
MaZderMind <github@mazdermind.de>
2012-12-28 14:44:21 +0100

    complete rewrite using apr methods this time with error checks and without leaks

--- src/mod_less.c
+++ src/mod_less.c
@@ -27,120 +27,264 @@
 #include "apr_strings.h"
 
 #include <stdio.h>
-#include <sys/stat.h>
 
+#define MAX_ERROR_SIZE 2*1024
 
-char static *source = NULL;
+int send_css_file(const char *filename, const apr_size_t size, request_rec *r) {
+	apr_status_t status;
+	apr_file_t *fd;
+	apr_size_t sentbytes;
 
-int file_exists(const char * filename)
-{
-    FILE * file = fopen(filename, "r");
-    if (file != NULL)
-    {
-        fclose(file);
-        return 1;
-    }
-    return 0;
-}
+	// open...
+	status = apr_file_open(&fd, filename, APR_READ | APR_BUFFERED | APR_SENDFILE_ENABLED, APR_OS_DEFAULT, r->pool);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_open failed while opening %s for read", filename);
+		return 0;
+	}
 
+	// ...send...
+	ap_set_content_type(r, "text/css");
+	status = ap_send_fd(fd, r, 0, size, &sentbytes);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "ap_send_fd failed while sending %s", filename);
+		return 0;
+	}
 
-int read_file(const char * filename){
-	long bufsize;
-	int count;
-	int error = 0;
-	FILE * fp = fopen(filename, "r");
-	if (fp != NULL) {
-		if (fseek(fp, 0L, SEEK_END) == 0) {
-			bufsize = ftell(fp);
-			if (bufsize == -1) 
-				error = -1 ;
-	
-			source = malloc(sizeof(char) * (bufsize + 1));
+	// ...and close
+	status = apr_file_close(fd);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_close failed while closing %s", filename);
+		return 0;
+	}
 
-			if (fseek(fp, 0L, SEEK_SET) == 0) 
-				error = -1; 
-
-			size_t newLen = fread(source, sizeof(char), bufsize, fp);
-
-			if (newLen == 0) {
-				error = -1;
-			} else {
-				source[++newLen] = '\0';
-			}
-		}
-
-	} 
-	fclose(fp);
-
-	if(error == -1) return 0;
 	return 1;
 }
 
+int close_and_delete(apr_file_t *fd, const char* file, request_rec *r) {
+	apr_status_t status;
 
-static int less_handler(request_rec* r)
-{
-	struct stat info;
-	struct stat lessinfo;
-	char * lessfilename = NULL;
-	char * command = NULL;
-	int status;
-	int newer = 0;
-	int error = 0;
-	char * errornum = NULL;
-
-	if (!r->handler || strcmp(r->handler, "less"))
-	return DECLINED;
-
-	if (r->method_number != M_GET)
-	return HTTP_METHOD_NOT_ALLOWED;
-
-	asprintf(&lessfilename, "%s.less", r->filename);
-	asprintf(&command, "lessc %s > %s", r->filename, lessfilename);
-
-	if (!file_exists(lessfilename)){
-
-		status = system(command);
-		if(status != 0)
-			return DECLINED;
-
+	status = apr_file_close(fd);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_close failed while closing %s", file);
+		return 0;
 	}
 
-	if (file_exists(lessfilename)){
+	status = apr_file_remove(file, r->pool);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_remove failed while removing %s", file);
+		return 0;
+	}
 
-		if ((stat(r->filename,&info) == 0) && (stat(lessfilename,&lessinfo) == 0) && (info.st_mtime > lessinfo.st_mtime))
-			status = system(command);
-		
-		if ((error = read_file(lessfilename)) != 1){
-			ap_set_content_type(r, "text/css");
-        		ap_rputs(source,r);
-			free(source);
+	return 1;
+}
+
+static int less_handler(request_rec* r) {
+	if (!r->handler || strcmp(r->handler, "less"))
+		return DECLINED;
+
+	if (r->method_number != M_GET)
+		return HTTP_METHOD_NOT_ALLOWED;
+
+	apr_status_t status;
+
+	// strip the extension of the filename in the request.
+	//   r->filename=foo.css or foo.less -> basename=foo
+	char *basename = strdup(r->filename);
+	if(basename == NULL) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "strdup returned NULL while building the files' basename");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	// find the last dot
+	char *pextension = strrchr(basename, '.');
+
+	// no dot found -> the filename is already the basename
+	if(pextension == NULL) {
+		pextension = basename;
+	}
+	else {
+		*pextension = '\0';
+	}
+
+	// build the paths to three files (css, less and tmp)
+	//  cssfile=foo.css, lessfile=foo.less
+	char *cssfile;
+	char *lessfile;
+	char *tmpfile;
+	if(asprintf(&lessfile, "%s.less", basename) == -1) {
+		free(basename);
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the less-filename");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+	if(asprintf(&cssfile, "%s.css", basename) == -1) {
+		free(basename);
+		free(lessfile);
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the css-filename");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+	if(asprintf(&tmpfile, "%s.css.tmpXXXXXX", basename) == -1) {
+		free(basename);
+		free(lessfile);
+		free(cssfile);
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the tmp-filename");
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+	free(basename);
+
+	apr_finfo_t lessinfo;
+	apr_finfo_t cssinfo;
+
+	// try to get the stat of the less-file. If it fails, there is no less-file.
+	// if there is no less-file corresponding to this css-file,
+	// it's probably a simple standalone css-file and should
+	// not be handled by this module
+	if(apr_stat(&lessinfo, lessfile, APR_FINFO_MTIME, r->pool) != APR_SUCCESS) {
+		free(lessfile);
+		free(cssfile);
+		return DECLINED;
+	}
+
+	// try to get the stat of the compiled css-file. If it succeeds, there is an already compiled-file.
+	// if there is a corresponding less-file and an already compiled
+	// css-file, stat them both and see, if the css-file is recent enough
+	if(apr_stat(&cssinfo, cssfile, APR_FINFO_MTIME | APR_FINFO_SIZE, r->pool) == APR_SUCCESS) {
+
+		// check, if the css-file is up-to-date
+		if(cssinfo.mtime > lessinfo.mtime) {
+
+			// send the css-file
+			if(!send_css_file(cssfile, cssinfo.size, r)) {
+				free(lessfile);
+				free(cssfile);
+				return HTTP_INTERNAL_SERVER_ERROR;
+			}
+
+			// and we're done here
+			free(lessfile);
+			free(cssfile);
 			return OK;
-
-		} else {
-
-			asprintf(&errornum, "%d", error);
-			ap_set_content_type(r, "text/css");
-        		ap_rputs(errornum,r);
-			free(source);
-			return OK;
-
 		}
 
 	}
-    	
+
+	// either there is not css-file or it is to old - in any way, we need to re-compile it
+	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "Compiling CSS-File %s from LESS-File %s via TMP-File %s", cssfile, lessfile, tmpfile);
+
+	// create the temp-file
+	apr_file_t *tmpfd;
+	status = apr_file_mktemp(&tmpfd, tmpfile, APR_CREATE | APR_READ | APR_WRITE | APR_EXCL, r->pool);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_mktemp failed while creating the temp-file: %s", tmpfile);
+
+		free(lessfile);
+		free(cssfile);
+		free(tmpfile);
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+
+	// TODO: place to config
+	//  placing the STDERR redirection before the STDOUT redirection gives us a
+	//  not-so-obvious result: sh connects lessc's STDERR with the popen-stream
+	//  and lessc's STDOUT with the named file - we get the errors and not the css-code
+	const char* lessc = "lessc --no-color %s 2>&1 >%s";
+
+	// construct the command to run
+	char* cmd;
+	if(asprintf(&cmd, lessc, lessfile, tmpfile) == -1) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while constructing the lessc-command out of %s, %s and %s", lessc, lessfile, cssfile);
+
+		close_and_delete(tmpfd, tmpfile, r);
+		free(lessfile);
+		free(cssfile);
+		free(tmpfile);
+
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	// execute lessc
+	//  this pipes the generated css-code into the tmpfile and the error into our popen-pipe
+	//  if the command succeeds, we'll move the tmpfile over the original css-file,
+	//  when lessc reports an error the tmpfile is meaningless and gets deleteds
+	//  TODO: error check
+	char err[MAX_ERROR_SIZE];
+	FILE* pp = popen(cmd, "r");
+	size_t bytes_read = fread(&err, 1, MAX_ERROR_SIZE-1, pp);
+	err[bytes_read] = '\0';
+	int ret = pclose(pp);
+
+	// check if lessc returned an error
+	if(ret != 0) {
+		ap_set_content_type(r, "text/plain");
+		ap_rputs(err, r);
+
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "lessc command '%s' returned %i, reporting '%s'", cmd, ret, err);
+
+		// close and delete the tmpfile
+		if(!close_and_delete(tmpfd, tmpfile, r))
+			return HTTP_INTERNAL_SERVER_ERROR;
+
+		free(lessfile);
+		free(cssfile);
+		free(tmpfile);
+		free(cmd);
+
+		// I would like to send a HTTP_INTERNAL_SERVER_ERROR but then apache ignores
+		// the sent error text and instead displays its own
+		return OK;
+	}
+	free(cmd);
+
+	// move the tmpfile over the cssfile
+	status = apr_file_rename(tmpfile, cssfile, r->pool);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_rename failed while moving the tmpfile %s over the cssfile %s", tmpfile, cssfile);
+
+		free(lessfile);
+		free(cssfile);
+		free(tmpfile);
+
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	// close the filepointer pointing to the tmpfile
+	// this removes the EXCL-lock on the file
+	status = apr_file_close(tmpfd);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_close failed while closing %s", tmpfile);
+
+		free(lessfile);
+		free(cssfile);
+		free(tmpfile);
+
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	free(tmpfile);
+	free(lessfile);
+
+	// send the css-file
+	if(!send_css_file(cssfile, cssinfo.size, r)) {
+		free(cssfile);
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	// and we're finally done
+	free(cssfile);
+	return OK;
 }
  
 static void register_hooks(apr_pool_t* pool)
 {
-    ap_hook_handler(less_handler, NULL, NULL, APR_HOOK_MIDDLE);
+	ap_hook_handler(less_handler, NULL, NULL, APR_HOOK_MIDDLE);
 }
  
 module AP_MODULE_DECLARE_DATA less_module = {
-    STANDARD20_MODULE_STUFF,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    register_hooks
+	STANDARD20_MODULE_STUFF,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	register_hooks
 };
