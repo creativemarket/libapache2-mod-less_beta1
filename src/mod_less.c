ad71088e481f9ebda5925f22d440f496a146790f
Chris Williams <chris@pureformsolutions.com>
2013-01-08 16:07:37 -0800

    Made the alt config permanent. Bypassed mtime checks -- in other words, ALWAYS compile less. Move the removal of the lock on the temp file to run just before renaming it. (prevents 'Text file busy' error). Corrected some grammatical errors :-)

--- src/mod_less.c
+++ src/mod_less.c
@@ -87,7 +87,7 @@ static int less_handler(request_rec* r) {
 
 	apr_status_t status;
 
-	// strip the extension of the filename in the request.
+	// strip the extension from the filename in the request.
 	//   r->filename=foo.css or foo.less -> basename=foo
 	char *basename = strdup(r->filename);
 	if(basename == NULL) {
@@ -113,20 +113,20 @@ static int less_handler(request_rec* r) {
 	char *tmpfile;
 	if(asprintf(&lessfile, "%s.less", basename) == -1) {
 		free(basename);
-		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the less-filename");
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the less filename");
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 	if(asprintf(&cssfile, "%s.css", basename) == -1) {
 		free(basename);
 		free(lessfile);
-		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the css-filename");
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the css filename");
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 	if(asprintf(&tmpfile, "%s.css.tmpXXXXXX", basename) == -1) {
 		free(basename);
 		free(lessfile);
 		free(cssfile);
-		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the tmp-filename");
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while formatting the tmp filename");
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 	free(basename);
@@ -134,9 +134,9 @@ static int less_handler(request_rec* r) {
 	apr_finfo_t lessinfo;
 	apr_finfo_t cssinfo;
 
-	// try to get the stat of the less-file. If it fails, there is no less-file.
-	// if there is no less-file corresponding to this css-file,
-	// it's probably a simple standalone css-file and should
+	// try to get the stat of the less file. If it fails, there is no less file.
+	// if there is no less file corresponding to this css file,
+	// it's probably a simple standalone css file and should
 	// not be handled by this module
 	if(apr_stat(&lessinfo, lessfile, APR_FINFO_MTIME, r->pool) != APR_SUCCESS) {
 		free(lessfile);
@@ -144,11 +144,11 @@ static int less_handler(request_rec* r) {
 		return DECLINED;
 	}
 
-	// try to get the stat of the compiled css-file. If it succeeds, there is an already compiled-file.
-	// if there is a corresponding less-file and an already compiled
-	// css-file, stat them both and see, if the css-file is recent enough
+	// try to get the stat of the compiled css file. If it succeeds, there is an already-compiled file.
+	// if there is a corresponding less file and an already-compiled css file,
+	// stat them both and see if the css file is recent enough to satisfy the request
 	if(apr_stat(&cssinfo, cssfile, APR_FINFO_MTIME | APR_FINFO_SIZE, r->pool) == APR_SUCCESS) {
-
+/*
 		// check, if the css-file is up-to-date
 		if(cssinfo.mtime > lessinfo.mtime) {
 
@@ -164,17 +164,17 @@ static int less_handler(request_rec* r) {
 			free(cssfile);
 			return OK;
 		}
-
+*/
 	}
 
-	// either there is not css-file or it is to old - in any way, we need to re-compile it
-	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "Compiling CSS-File %s from LESS-File %s via TMP-File %s", cssfile, lessfile, tmpfile);
+	// either there is no css file or it is too old - either way we need to recompile it
+	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "Compiling CSS File %s from LESS File %s via TMP File %s", cssfile, lessfile, tmpfile);
 
-	// create the temp-file
+	// create the temp file
 	apr_file_t *tmpfd;
 	status = apr_file_mktemp(&tmpfd, tmpfile, APR_CREATE | APR_READ | APR_WRITE | APR_EXCL, r->pool);
 	if(status != APR_SUCCESS) {
-		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_mktemp failed while creating the temp-file: %s", tmpfile);
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_mktemp failed while creating the temp file: %s", tmpfile);
 
 		free(lessfile);
 		free(cssfile);
@@ -185,14 +185,14 @@ static int less_handler(request_rec* r) {
 
 	// TODO: place to config
 	//  placing the STDERR redirection before the STDOUT redirection gives us a
-	//  not-so-obvious result: sh connects lessc's STDERR with the popen-stream
-	//  and lessc's STDOUT with the named file - we get the errors and not the css-code
+	//  not-so-obvious result: sh connects lessc's STDERR with the popen stream
+	//  and lessc's STDOUT with the named file - we get the errors and not the css code
 	const char* lessc = "lessc --no-color %s 2>&1 >%s";
 
 	// construct the command to run
 	char* cmd;
 	if(asprintf(&cmd, lessc, lessfile, tmpfile) == -1) {
-		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while constructing the lessc-command out of %s, %s and %s", lessc, lessfile, cssfile);
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, 0, r, "asprintf failed while constructing the lessc command out of %s, %s and %s", lessc, lessfile, cssfile);
 
 		close_and_delete(tmpfd, tmpfile, r);
 		free(lessfile);
@@ -203,9 +203,9 @@ static int less_handler(request_rec* r) {
 	}
 
 	// execute lessc
-	//  this pipes the generated css-code into the tmpfile and the error into our popen-pipe
-	//  if the command succeeds, we'll move the tmpfile over the original css-file,
-	//  when lessc reports an error the tmpfile is meaningless and gets deleteds
+	//  this pipes the generated css code into the tmpfile and the error into our popen pipe
+	//  if the command succeeds, we'll move the tmpfile over the original css file,
+	//  when lessc reports an error the tmpfile is meaningless and gets deleted
 	//  TODO: error check
 	char err[MAX_ERROR_SIZE];
 	FILE* pp = popen(cmd, "r");
@@ -229,11 +229,24 @@ static int less_handler(request_rec* r) {
 		free(tmpfile);
 		free(cmd);
 
-		// I would like to send a HTTP_INTERNAL_SERVER_ERROR but then apache ignores
+		// I would like to send a HTTP_INTERNAL_SERVER_ERROR but apache ignores
 		// the sent error text and instead displays its own
 		return OK;
 	}
 	free(cmd);
+
+	// close the filepointer pointing to the tmpfile
+	// this removes the EXCL lock on the file
+	status = apr_file_close(tmpfd);
+	if(status != APR_SUCCESS) {
+		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_close failed while closing %s", tmpfile);
+
+		free(lessfile);
+		free(cssfile);
+		free(tmpfile);
+
+		return HTTP_INTERNAL_SERVER_ERROR;
+	}
 
 	// move the tmpfile over the cssfile
 	status = apr_file_rename(tmpfile, cssfile, r->pool);
@@ -247,23 +260,10 @@ static int less_handler(request_rec* r) {
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 
-	// close the filepointer pointing to the tmpfile
-	// this removes the EXCL-lock on the file
-	status = apr_file_close(tmpfd);
-	if(status != APR_SUCCESS) {
-		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_file_close failed while closing %s", tmpfile);
-
-		free(lessfile);
-		free(cssfile);
-		free(tmpfile);
-
-		return HTTP_INTERNAL_SERVER_ERROR;
-	}
-
 	free(tmpfile);
 	free(lessfile);
 
-	// re-stat the css-file to get the new size
+	// re-stat the css file to get the new size
 	status = apr_stat(&cssinfo, cssfile, APR_FINFO_SIZE, r->pool);
 	if(status != APR_SUCCESS) {
 		ap_log_rerror(APLOG_MARK, APLOG_CRIT, status, r, "apr_stat failed while stat'ing %s", cssfile);
@@ -273,7 +273,7 @@ static int less_handler(request_rec* r) {
 		return HTTP_INTERNAL_SERVER_ERROR;
 	}
 
-	// send the css-file
+	// send the css file
 	if(!send_css_file(cssfile, cssinfo.size, r)) {
 		free(cssfile);
 		return HTTP_INTERNAL_SERVER_ERROR;
